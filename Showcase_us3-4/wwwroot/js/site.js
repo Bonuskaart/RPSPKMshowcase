338f1573b23ab7f8f51cc9755c6024b3ad8ad6d0
Bonuskaart <90609753+Bonuskaart@users.noreply.github.com>
2024-03-14 20:25:39 +0100

    eerste stppen, buttons, opmaak

--- Showcase_us3-4/wwwroot/js/site.js
+++ Showcase_us3-4/wwwroot/js/site.js
@@ -1,15 +1,6 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿
+//element.addEventListener("click", myFunction);
 
-// Write your JavaScript code.
-    class HelloWorld extends HTMLElement {
-        constructor() {
-        super();
-    this.attachShadow({mode: 'open' });
-    this.shadowRoot.innerHTML = `
-    <h1>Hello world!</h1>
-    `;
-            }
-        }
-
-    customElements.define('hello-world', HelloWorld);
+//function myFunction() {
+//    ;
+//}
