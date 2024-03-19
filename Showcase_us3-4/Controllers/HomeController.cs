062c00ee143e09c468e6a5634e52face598413fc
Bonuskaart <90609753+Bonuskaart@users.noreply.github.com>
2024-03-19 17:02:02 +0100

    eind development us1

--- Showcase_us3-4/Controllers/HomeController.cs
+++ Showcase_us3-4/Controllers/HomeController.cs
@@ -22,7 +22,10 @@ namespace Showcase_us3_4.Controllers
         {
             return View();
         }
-
+        public IActionResult Battleview()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
