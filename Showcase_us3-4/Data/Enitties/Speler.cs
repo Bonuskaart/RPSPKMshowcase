930f9d9be5b4850f41c91c86b27b240d8dffdc95
Bonuskaart <90609753+Bonuskaart@users.noreply.github.com>
2024-03-19 09:04:09 +0100

    docker

--- Showcase_us3-4/Data/Enitties/Speler.cs
+++ Showcase_us3-4/Data/Enitties/Speler.cs
@@ -5,7 +5,8 @@ namespace Showcase_us3_4.Data.Enitties
     public class Speler : IdentityUser
     {
         public string Username { get; set; }
-        public int SpelerId { get; set; }
+        //public int SpelerId { get; set; }
+        public string Email { get; set;}
 
     }
 }
