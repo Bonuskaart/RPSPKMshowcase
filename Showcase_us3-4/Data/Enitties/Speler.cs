e8270556f00cd883abebc206337217983806b296
Bonuskaart <90609753+Bonuskaart@users.noreply.github.com>
2024-03-20 10:00:06 +0100

    steenpapierschaarTestFailed

--- Showcase_us3-4/Data/Enitties/Speler.cs
+++ Showcase_us3-4/Data/Enitties/Speler.cs
@@ -5,8 +5,7 @@ namespace Showcase_us3_4.Data.Enitties
     public class Speler : IdentityUser
     {
         public string Username { get; set; }
-        //public int SpelerId { get; set; }
-        public string Email { get; set;}
+        public int SpelerId { get; set; }
 
     }
 }
